c02c137b62cede5aa092ca5eeac2b7d7cadbebd0
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-03-19 00:54:39 +0100

    Nicer UI in item editor

---
+++ ResourceManifest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orchard.UI.Resources;
+
+namespace Piedone.ContentWidgets
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+            manifest.DefineStyle("ContentWidgetsAdmin").SetUrl("piedone-content-widgets-admin.css");
+        }
+    }
+}
