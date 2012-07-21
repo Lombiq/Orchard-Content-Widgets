615307c7c06ec9423dad3ab5473bc205d06ff7fb
Piedone <piedone@pyrocenter.hu>
2012-07-21 11:25:07 +0200

    Fixing migrations bad naming

--- Migrations.cs
+++ Migrations.cs
@@ -2,22 +2,40 @@
 using Orchard.Core.Contents.Extensions;
 using Orchard.Data.Migration;
 using Piedone.ContentWidgets.Models;
+using Orchard.Data.Migration.Records;
+using Orchard.Data;
+using System.Linq;
 
 namespace Piedone.ContentWidgets
 {
-    // Funny fail of copy-paste :-)
-    public class FacebookLikeButtonMigrations : DataMigrationImpl
+    public class Migrations : DataMigrationImpl
     {
+        private readonly IRepository<DataMigrationRecord> _migrationRepository;
+
+        public Migrations(IRepository<DataMigrationRecord> migrationRepository)
+        {
+            _migrationRepository = migrationRepository;
+        }
+
         public int Create()
         {
-            SchemaBuilder.CreateTable(typeof(ContentWidgetsPartRecord).Name,
-                table => table
-                    .ContentPartRecord()
-                    .Column<string>("ExcludedWidgetIdsDefinition")
-            );
+            var existingMigration = _migrationRepository.Fetch(record => record.DataMigrationClass == "Piedone.ContentWidgets.FacebookLikeButtonMigrations").FirstOrDefault();
 
-            ContentDefinitionManager.AlterPartDefinition(typeof(ContentWidgetsPart).Name,
-                builder => builder.Attachable());
+            if (existingMigration != null)
+            {
+                _migrationRepository.Delete(existingMigration);
+            }
+            else
+            {
+                SchemaBuilder.CreateTable(typeof(ContentWidgetsPartRecord).Name,
+                        table => table
+                            .ContentPartRecord()
+                            .Column<string>("ExcludedWidgetIdsDefinition", column => column.Unlimited())
+                    );
+
+                ContentDefinitionManager.AlterPartDefinition(typeof(ContentWidgetsPart).Name,
+                    builder => builder.Attachable());
+            }
 
 
             return 1;
